a6bfff6b8cd2b0696854ade2ec9cc24a6649f618
BinayBhagat <binaybhagat@gmail.com>
2025-04-24 22:26:12 -0700

    commit

---
+++ Python/Hacker_Rank/utopian-tree.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    output=1
+    if n==0:
+        return output
+    for i in range(1,n+1):
+        if(i%2 == 0):
+            output+=1
+        else:
+            output=output*2
+        #print(f"n -->{i}, output --> {output}")
+    return output
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
